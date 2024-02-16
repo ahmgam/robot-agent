f84ca10e8087c8ca088e875eff5a63c5a37c60ce
ahmgam <ahmgam@protonmail.com>
2024-02-16 23:07:48 +0200

    Update launch file with correct base directory path

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,6 @@ set -e
 source "/opt/ros/noetic/setup.bash" --
 source "/robot_ws/devel/setup.bash" --
 #roslaunch rotors_gazebo uav_launch.launch
-roslaunch --wait --local -v /robot_ws/src/multirobot_sim/launch/blockchain.launch
+roslaunch  -v /robot_ws/src/multirobot_sim/launch/blockchain.launch > /dev/stdout 2> /dev/stderr
 
 exec "$@"
